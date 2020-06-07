aafd402b918544c24cddd9d9b0ceff63cdb56f77
Salumão Barbosa da Costa <salubcosta@gmail.com>
2020-06-06 23:14:13 -0300

    updated file

--- PYTHON/01-random.py
+++ PYTHON/01-random.py
@@ -1,13 +1,19 @@
 from random import randint
 
-number = randint(0, 10)
+number = randint(0, 100)
 
+print('##10 chances##')
+
+qtd = 0
 while 1 == 1:
-    choice = int(input('Chute um número (0a10): '))
-
+    choice = int(input('Chute um número (0 a 100): '))
+    qtd += 1
     if(number == choice):
         print('Acertou!')
         break
     else:
-        print('Errou!')
-	pass
+        if qtd == 10:
+            print('Game over!')
+            break
+        tip = 'Tente um número maior' if choice < number else 'Tente um número menor'
+        print('Errou!', tip)
