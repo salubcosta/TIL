a3bb0fe2ba9972734a504d17b9e6b7210b7fdbe9
Salumão Barbosa da Costa <salubcosta@gmail.com>
2020-06-06 23:14:55 -0300

    created file loop for

---
+++ PYTHON/02-for.py
@@ -0,0 +1,13 @@
+for x in range(0,100,25):
+    print(x)
+
+for x in range(0,100):
+    print(x)
+
+for x in range(100):
+    print(x)
+
+for x in range(100):
+    print(x)
+else:
+    print('finalizou')
